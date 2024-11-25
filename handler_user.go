ac2a2e7095474aae55de87d4f2a9e65e9f1293e6
haider <haidersultan7675@gmail.com>
2024-11-25 19:26:36 +0500

    Added User Model And Create User Post Request

---
+++ handler_user.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/CTZNpk/rssaggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiConfig *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `name`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithEror(w, 400, fmt.Sprintf("Error parsing JSON:", err))
+		return
+	}
+
+	user, err := apiConfig.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithEror(w, 400, fmt.Sprintf("Couldn't create user:", err))
+		return
+	}
+	respondWithJson(w, 200, databaseUserToUser(user))
+}
--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/CTZNpk/rssaggregator/internal/database"
 	"github.com/joho/godotenv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func respondWithEror(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
@@ -46,16 +53,31 @@ func main() {
 		Addr:    ":8000",
 	}
 
-	v1Router := chi.NewRouter()
-	v1Router.Get("/ready", handlerRediness)
-	v1Router.Get("/err", handlerErr)
-
-	router.Mount("/v1", v1Router)
-
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT KEY NOT FOUND")
 	}
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL was not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerRediness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+
+	router.Mount("/v1", v1Router)
 
 	fmt.Println(portString)
 	fmt.Println("The Server is listening")
