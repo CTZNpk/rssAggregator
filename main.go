073e02877eb7d1e656041eb1e06ca8b7e545520c
haider <haidersultan7675@gmail.com>
2024-11-25 14:06:29 +0500

    Tested basic server

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+)
 
-func main(){
-  fmt.Println("Hello World")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World hahah ")
+
+}
+
+func main() {
+
+	godotenv.Load()
+
+	http.HandleFunc("/", handler)
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT KEY NOT FOUND")
+	}
+
+	fmt.Println(portString)
+  fmt.Println("The Server is listening")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
